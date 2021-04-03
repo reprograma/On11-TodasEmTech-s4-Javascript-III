df72e6cb81e01e238022cc60a0326fb7de7c50bf
Raphaella <raphaa_leite@hotmail.com>
2021-04-03 10:42:16 -0300

    Desafios da semana

---
+++ testeArray.js
@@ -0,0 +1,14 @@
+var itensRapha = ['Livro','jornal' , 'Revista', 'Gibis', 'Cartas', 'Podcast' , 'Radio']
+
+alert(itensRapha[5])//imprime apenas o elemento 5
+alert(itensRapha.length) //Le qntos itens tem 
+alert(itensRapha) //Imprime a lista
+
+//Uma escola precisa montar a lista de alunos do 5º B (5 alunos). Crie um programa que receba CADA nome de aluno e imprima na tela em ordem alfabetica todos os alunos desta lista.
+for (var i = 1; i<=10; i++){ //10 numero de alunos,repetir prompt ate ter 10 alunos cadastrados
+
+var inprompt = prompt("Digite o nome dos alunos")
+}
+
+var listAlunos = [inprompt]
+alert(listAlunos)
